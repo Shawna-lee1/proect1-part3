e6f6c00a56fa9706256bf556dff11a3003ae4319
Shawna-lee1 <104038577+Shawna-lee1@users.noreply.github.com>
2023-10-08 01:26:01 -0400

    Update client.py

--- client.py
+++ client.py
@@ -1,26 +1,47 @@
 import socket
+import sys
 
 # Define the server's host and port
 server_host = 'localhost'  # Change to the actual server IP if needed
 server_port = 12345
 
+# Define the file to send (change as needed)
+file_to_send = "sample.txt"
+
+def send_file_data(client_socket):
+    try:
+        with open(file_to_send, "rb") as file:
+            while True:
+                data = file.read(4096)
+                if not data:
+                    break
+                client_socket.send(data)
+    except Exception as e:
+        print(f"Error sending file data: {e}")
+
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect to the server
-client_socket.connect((server_host, server_port))
+try:
+    # Connect to the server
+    client_socket.connect((server_host, server_port))
 
-# Receive the welcome message from the server
-welcome_message = client_socket.recv(1024)
-print(welcome_message.decode())
+    # Receive the welcome message from the server
+    welcome_message = client_socket.recv(1024)
+    print(welcome_message.decode())
 
-# Send a message to the server
-message = "Hello, server!"
-client_socket.send(message.encode())
+    # Send the file data to the server
+    send_file_data(client_socket)
 
-# Receive a response from the server
-response = client_socket.recv(1024)
-print(f"Server response: {response.decode()}")
+    # Close the client socket
+    client_socket.close()
 
-# Close the client socket
-client_socket.close()
+except ConnectionRefusedError:
+    print("ERROR: Server is not running or refused the connection.")
+except FileNotFoundError:
+    print(f"ERROR: File '{file_to_send}' not found.")
+except KeyboardInterrupt:
+    print("Client terminated by user.")
+except Exception as e:
+    print(f"Error: {e}")
+
