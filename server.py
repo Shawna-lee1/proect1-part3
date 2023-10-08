1a59e3580107fab503e7b737c0d8f1ca29100426
Shawna-lee1 <104038577+Shawna-lee1@users.noreply.github.com>
2023-10-07 22:58:33 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,41 +1,104 @@
+import os
+import sys
 import socket
+import threading
+import signal
+import time
 
-host = '0.0.0.0'  # Listen on all available network interfaces
-port = 12345     
+# Global variables to track connection count and file directory
+connection_count = 0
+file_dir = ""
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the host and port
-server_socket.bind((host, port))
-
-# Listen for incoming connections
-server_socket.listen(5)  # Listen for up to 5 connections
-
-print(f"Server is listening on {host}:{port}")
-
-while True:
-    # Accept a client connection
-    client_socket, client_address = server_socket.accept()
-    print(f"Accepted connection from {client_address}")
+# Function to handle each client connection
+def handle_connection(client_socket, connection_id):
+    global file_dir
     
-    # Send a welcome message to the client
-    message = "Welcome to the server!"
-    client_socket.send(message.encode())
+    # Create a file path for the current connection
+    file_path = os.path.join(file_dir, f"{connection_id}.file")
     
-    # Receive data from the client
-    data = client_socket.recv(1024)
-    if not data:
-        break
+    try:
+        # Set a timeout for the connection
+        client_socket.settimeout(10)
+        
+        # Send the "accio" command to the client
+        client_socket.send(b"accio\r\n")
+        
+        # Open the file for writing in binary mode
+        with open(file_path, "wb") as file:
+            while True:
+                data = client_socket.recv(4096)
+                if not data:
+                    break
+                
+                # Write the received data to the file
+                file.write(data)
+                
+        print(f"Connection {connection_id} received and saved.")
     
-    print(f"Received data from client: {data.decode()}")
+    except socket.timeout:
+        # Handle timeout (no data received for 10 seconds)
+        error_msg = "ERROR"
+        with open(file_path, "wb") as file:
+            file.write(error_msg.encode())
+        print(f"Connection {connection_id} timed out and saved an error message.")
     
-    # Send a response back to the client
-    response = "Server received your message."
-    client_socket.send(response.encode())
+    except Exception as e:
+        print(f"Error in connection {connection_id}: {e}")
     
-    # Close the client socket
-    client_socket.close()
+    finally:
+        # Close the client socket
+        client_socket.close()
 
-# Close the server socket when done
-server_socket.close()
+# Function to gracefully handle signals
+def signal_handler(signal, frame):
+    print("Received signal, exiting gracefully...")
+    sys.exit(0)
+
+# Main function
+def main():
+    global connection_count, file_dir
+    
+    # Check command-line arguments
+    if len(sys.argv) != 3:
+        sys.stderr.write("ERROR: Usage: python3 server.py <PORT> <FILE-DIR>\n")
+        sys.exit(1)
+    
+    port = int(sys.argv[1])
+    file_dir = sys.argv[2]
+    
+    # Validate the FILE-DIR path
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    
+    # Register signal handlers for graceful exit
+    signal.signal(signal.SIGQUIT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+    
+    # Create a listening socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("0.0.0.0", port))
+    server_socket.listen(10)  # Allow up to 10 pending connections
+    
+    print(f"Server is listening on port {port}")
+    
+    try:
+        while True:
+            # Accept a new connection
+            client_socket, addr = server_socket.accept()
+            
+            # Increment the connection count
+            connection_count += 1
+            
+            # Start a new thread to handle the connection
+            t = threading.Thread(target=handle_connection, args=(client_socket, connection_count))
+            t.start()
+    
+    except KeyboardInterrupt:
+        print("Server terminated by user.")
+    
+    finally:
+        server_socket.close()
+
+if __name__ == "__main__":
+    main()
